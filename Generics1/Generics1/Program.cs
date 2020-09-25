a76a22579fb98087b26aded6e5022c56be3962af
Djalma <djalma.ant@gmail.com>
2020-09-24 21:33:11 -0300

    Generics1

--- Generics1/Generics1/Program.cs
+++ Generics1/Generics1/Program.cs
@@ -6,7 +6,19 @@ namespace Generics1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintService ps = new PrintService();
+            
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                ps.AddValue(x);
+            }
+
+            ps.Print();
+            Console.WriteLine("First: " + ps.First());
         }
     }
 }
