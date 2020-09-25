987b0c0f0b2fdf7a079e747292763134f6ebc6a1
Djalma <djalma.ant@gmail.com>
2020-09-24 21:14:39 -0300

    Initial Git

---
+++ Generics1/Generics1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
