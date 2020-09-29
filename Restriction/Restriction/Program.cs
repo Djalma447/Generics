9af903b3cf54cc535c0d6d0e5b6d180393c624a6
Djalma <djalma.ant@gmail.com>
2020-09-28 23:58:24 -0300

    Restriction

--- Restriction/Restriction/Program.cs
+++ Restriction/Restriction/Program.cs
@@ -1,6 +1,8 @@
-﻿using Restriction.Services;
+﻿using Restriction.Entities;
+using Restriction.Services;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace Restriction
 {
@@ -8,19 +10,21 @@ namespace Restriction
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>();
+            List<Product> list = new List<Product>();
             
             Console.Write("Enter N: ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
             {
-                int x = int.Parse(Console.ReadLine());
-                list.Add(x);
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
             }
 
             CalculationService calculationService = new CalculationService();
-            int max = calculationService.Max<int>(list);
+            Product max = calculationService.Max(list);
 
             Console.WriteLine("Max:");
             Console.WriteLine(max);
