1e0b2453c8465b542ede78b733636548a79c416d
Djalma <djalma.ant@gmail.com>
2020-10-04 20:37:57 -0300

    Sets First Demo HashSet

---
+++ Sets/Sets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+            
+            Console.WriteLine(set.Contains("Notebook"));
+            Console.WriteLine(set.Contains("Computer"));
+
+            Console.WriteLine();
+
+            foreach(string p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
